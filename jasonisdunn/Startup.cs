9da564bdb9a896bdaaedc254e8bbf98d0598b817
Jason Dunn <jason.dunn@jasonisdunn.tech>
2021-10-06 12:53:28 -0600

    Create Toggle

--- jasonisdunn/Startup.cs
+++ jasonisdunn/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MudBlazor.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -43,6 +44,7 @@ namespace jasonisdunn
             }
 
             services.AddSingleton<WeatherForecastService>();
+            services.AddMudServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
